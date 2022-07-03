62782f4cb72b8cbc768b4cd54c7dfe1ba9d60d4b
Txc <1627764899@qq.com>
2022-07-03 12:54:00 +0800

    1.添加generator代码生成器

---
+++ microservice-api/src/main/java/com/study/utils/Generator.java
@@ -0,0 +1,44 @@
+package com.study.utils;
+
+/**
+ * @className: Genarator
+ * @description: TODO 类描述
+ * @author: Txc
+ * @date: 2022/7/3 12:49
+ **/
+public class Generator {
+
+    /* 数据库连接信息 */
+    private static final String jdbcDiverClassName = "";
+    private static final String url = "jdbc:mysql://localhost:3306/testtxc";
+    private static final String userName = "root";
+    private static final String passWord = "123456";
+
+    /* 导出的数据库名 */
+    private static final String tableName = "";
+
+    /* 文件输出位置 */
+    private static final String outPutFileDir = "";
+
+    /* 模块名称 */
+    private static final String moduleName = "";
+
+    /* 类注解信息 */
+    private static final String author = "Txc";
+
+    /* 父级包名 */
+    private static final String parentPackage = "com.study";
+
+    /* 包名 */
+    private static final String controllerName = "Controller";
+    private static final String serviceName = "service";
+    private static final String serviceImplName = "service.serviceImpl";
+    private static final String daoName = "dao";
+    private static final String mapperName = "mapper";
+    private static final String pojo = "entity";
+
+    public static void main(String[] args) {
+
+    }
+
+}
