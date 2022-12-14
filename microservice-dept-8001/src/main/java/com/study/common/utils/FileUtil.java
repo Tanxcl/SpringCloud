8c302497bbc29764ce6bbb179c5af5c0ee849e03
Txc <1627764899@qq.com>
2022-12-15 01:21:35 +0800

    完善文件上传下载工具类

--- microservice-dept-8001/src/main/java/com/study/common/utils/FileUtil.java
+++ microservice-dept-8001/src/main/java/com/study/common/utils/FileUtil.java
@@ -1,5 +1,15 @@
 package com.study.common.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
 /**
  * @className: FileUtil
  * @description: 文件上传下载工具
@@ -7,4 +17,120 @@ package com.study.common.utils;
  * @date: 2022/7/3 23:54
  **/
 public class FileUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+    /**
+     * 单文件上传
+     */
+    public String upload(MultipartFile file) {    //注意参数
+        try {
+            if (file.isEmpty()) {
+                return "文件为空";
+            }
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            logger.info("上传的文件名为：" + fileName);//写日志
+            // 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            logger.info("文件的后缀名为：" + suffixName);//写日志
+            // 设置文件存储路径         *************************************************
+            String filePath = "./FILE/KING/";
+            String path = filePath + fileName;
+            File dest = new File(new File(path).getAbsolutePath());// dist为文件，有多级目录的文件
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {//因此这里使用.getParentFile()，目的就是取文件前面目录的路径
+                dest.getParentFile().mkdirs();// 新建文件夹
+            }
+            file.transferTo(dest);// 文件写入
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+    /**
+     * 多文件上传
+     */
+    public String handleFileUpload(HttpServletRequest request) {   //注意参数
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        for (int i = 0; i < files.size(); ++i) {
+            file = files.get(i);
+            String filePath = "./FILE/KING/";            // 文件路径
+            File dest = new File(filePath);
+            // 检测是否存在目录
+            if (!dest.exists()) {
+                dest.mkdirs();// 新建文件夹
+            }
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    stream = new BufferedOutputStream(new FileOutputStream(
+                            new File(filePath + file.getOriginalFilename())));//设置文件路径及名字
+                    stream.write(bytes);// 写入
+                    stream.close();
+                } catch (Exception e) {
+                    stream = null;
+                    return "第 " + i + " 个文件上传失败 ==> " + e.getMessage();
+                }
+            } else {
+                return "第 " + i + " 个文件上传失败因为文件为空";
+            }
+        }
+        return "上传成功";
+    }
+
+    /**
+     * 下载文件
+     */
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "png.png";// 文件名
+        if (fileName != null) {
+            //设置文件路径
+            File file = new File("./FILE/KING/png.png");
+            //File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally { // 做关闭操作
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
+
 }
