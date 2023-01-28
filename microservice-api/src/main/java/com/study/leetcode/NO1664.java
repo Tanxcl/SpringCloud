31a7ea6206c1641b9a1186ca382d7137bd332da9
OrangeCheng <1627764899@qq.com>
2023-01-28 21:51:08 +0800

    LeetCode

---
+++ microservice-api/src/main/java/com/study/leetcode/NO1664.java
@@ -0,0 +1,21 @@
+package com.study.leetcode;
+
+/**
+ * @ClassName: NO1664
+ * @description: 生成平衡数组的方案数
+ * @author: OrangeCheng
+ * @create: 2023-01-28 21:31
+ **/
+public class NO1664 {
+
+    public static void main(String[] args) {
+        int[] a = {};
+        NO1664 no1664 = new NO1664;
+        int i = no1664.waysToMakeFair(a);
+        System.out.println(i);
+    }
+
+    public int waysToMakeFair(int[] nums) {
+
+    }
+}
